05a02666be7684e7f97b2821886fd5ea064c23ac
Mert Duzgun <duzgunnmert@gmail.com>
2021-01-06 22:49:08 +0300

    Add key-sequence-detection project

---
+++ key-sequence-detection/index.js
@@ -0,0 +1,28 @@
+// my solution before watching the whole video
+
+// let pressed = [];
+// const secretCode = 'trymeEnter';
+
+// window.addEventListener('keyup', (e) => {
+//   pressed.push(e.key);
+//   if (e.key == 'Enter') {
+//     const currentCombination = pressed.join('');
+//     if (currentCombination === secretCode) {
+//       alert('congrats');
+//     }
+//     pressed = [];
+//   }
+// });
+
+// Wes' solution
+
+const pressed = [];
+const secretCode = 'wesbos';
+
+window.addEventListener('keyup', (e) => {
+  pressed.push(e.key);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+  if (pressed.join('').includes(secretCode)) {
+    cornify_add();
+  }
+});
